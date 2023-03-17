7a8d4257196189394151a68f36383ed1bcc6b052
myantyuWorld <nepinepi984@gmail.com>
2023-03-17 10:27:53 +0900

    Add experience_technology api

--- aws-sam-api/experience_technology/src/app.py
+++ aws-sam-api/experience_technology/src/app.py
@@ -9,15 +9,27 @@ import boto3
 # client = boto3.client('dynamodb', endpoint_url = "http://dynamodb-local:8000")
 client = boto3.client('dynamodb')
 
+# curl "http://localhost:3000/experience_technology" -X POST -d '{"user_id":"myantyuWorld", "name": "C#", "category" : "1", "level":"3"}'
 def post_handler(event, context):
     # リクエストbody取得
     body = json.loads(event["body"])
     name = body["name"]
     # db登録
-    result = client.put_item(TableName="members", Item={"name": {"S": name}})
+    result = client.put_item(TableName="experience_technologies", Item={"name": {"S": name}})
     response = {
         "statusCode": 200,
         "body": json.dumps(result)
+    }
+
+    return response
+
+# curl "http://localhost:3000/experience_technology"
+def get_handler(event, context):
+    result = client.scan(TableName="experience_technologies")   
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result["Items"])
     }
 
     return response
