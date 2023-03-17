4410e9c578e676c8ff73e5aa62ea407a8cbe67a2
myantyuWorld <nepinepi984@gmail.com>
2023-03-17 10:14:17 +0900

    技術マスタAPI jsonを整形

--- aws-sam-api/technology/src/app.py
+++ aws-sam-api/technology/src/app.py
@@ -4,12 +4,11 @@ import boto3
 client = boto3.client('dynamodb')
 
 def handler(event, context):
-    # TODO : 整形して返す
     result = client.scan(TableName="technologies")   
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(result)
+        "body": json.dumps(result["Items"])
     }
 
     return response
