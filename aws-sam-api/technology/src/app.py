918f7704640f3d0ecb9f57b58ac8cd185a30dd13
myantyuWorld <nepinepi984@gmail.com>
2023-03-29 22:47:34 +0900

    cors 暫定対応

--- aws-sam-api/technology/src/app.py
+++ aws-sam-api/technology/src/app.py
@@ -1,7 +1,10 @@
 import json
 import boto3
 import ast
+from boto3.dynamodb.conditions import Key, Attr
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('technologies')
 client = boto3.client('dynamodb')
 
 '''
@@ -27,8 +30,15 @@ client = boto3.client('dynamodb')
 '''
 def handler(event, context):
     result = client.scan(TableName="technologies")   
+    # res = table.query(KeyConditionExpression=Key('category').eq('1')) # Lang
+    headers = {
+        'Access-Control-Allow-Headers' : 'Content-Type',
+        'Access-Control-Allow-Origin'  : '*',
+        'Access-Control-Allow-Methods' : 'GET'
+    }
     response = {
         "statusCode": 200,
+        'headers': headers,
         "body": json.dumps(result["Items"])
     }
 
